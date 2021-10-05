0ddea5d1902619133519d7cbdff954c0e108efe4
IV1T3 <alexander.groddeck@uni-hamburg.de>
2021-10-05 19:55:31 +0200

    Removed unnecessary data

--- django_fileuploadvalidation/modules/validation/ms_office.py
+++ django_fileuploadvalidation/modules/validation/ms_office.py
@@ -10,65 +10,24 @@ def check_vba_macros(file):
 
     malicious_indicators = {
         "AutoExec": [],
+        "Suspicious": [],
         "IOC": [],
         "Hex String": [],
         "Base64 String": [],
-        "AutoExec": [],
-        "Suspicious": [],
         "Dridex String": [],
+        "VBA String": [],
         "MacroRaptor": [],
-        "nb_counts": {
-            "AutoExec": 0,
-            "Suspicious": 0,
-            "IOC": 0,
-            "Hex String": 0,
-            "Base64 String": 0,
-            "Dridex String": 0,
-            "VBA String": 0,
-        },
     }
 
     macros_found = vba_parser.detect_macros()
     if macros_found:
 
-        macro_analysis_results = vba_parser.analyze_macros(show_decoded_strings=True)
-        for key_type, key_value, desc in macro_analysis_results:
-            malicious_indicators[key_type].append([key_value, desc])
-
         # Iterating all indicators according to:
         # https://github.com/decalage2/oletools/blob/08056c175b3b3bb2a0271f1c9601c0c9e47ad1b8/oletools/olevba.py
 
-        # AutoExec keywords
-        if vba_parser.nb_autoexec > 0:
-            malicious_indicators["nb_counts"]["AutoExec"] = vba_parser.nb_autoexec
-
-        # Suspicious keywords
-        if vba_parser.nb_suspicious > 0:
-            malicious_indicators["nb_counts"]["Suspicious"] = vba_parser.nb_suspicious
-
-        # Indicator of Compromise
-        if vba_parser.nb_iocs > 0:
-            malicious_indicators["nb_counts"]["IOC"] = vba_parser.nb_iocs
-
-        # Hex obfuscated strings
-        if vba_parser.nb_hexstrings > 0:
-            malicious_indicators["nb_counts"]["Hex String"] = vba_parser.nb_hexstrings
-
-        # Base64 obfuscated strings
-        if vba_parser.nb_base64strings > 0:
-            malicious_indicators["nb_counts"][
-                "Base64 String"
-            ] = vba_parser.nb_base64strings
-
-        # Dridex obfuscated strings
-        if vba_parser.nb_dridexstrings > 0:
-            malicious_indicators["nb_counts"][
-                "Dridex String"
-            ] = vba_parser.nb_dridexstrings
-
-        # VBA obfuscated strings
-        if vba_parser.nb_vbastrings > 0:
-            malicious_indicators["nb_counts"]["VBA String"] = vba_parser.nb_vbastrings
+        macro_analysis_results = vba_parser.analyze_macros(show_decoded_strings=True)
+        for key_type, key_value, desc in macro_analysis_results:
+            malicious_indicators[key_type].append([key_value, desc])
 
         # Analyzing macros with MacroRaptor
         vba_code_all_modules = ""
