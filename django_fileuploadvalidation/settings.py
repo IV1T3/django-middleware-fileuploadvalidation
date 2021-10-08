130d0bcda53af6fcb62dee7e4ac28d480a5751e6
IV1T3 <alexander.groddeck@uni-hamburg.de>
2021-10-08 15:32:10 +0200

    Remove code duplicates

--- django_fileuploadvalidation/settings.py
+++ django_fileuploadvalidation/settings.py
@@ -5,16 +5,5 @@ from django.conf import settings
 UPLOAD_CONFIGURATION = getattr(
     settings,
     "UPLOAD_CONFIGURATION",
-    {
-        "default": {
-            "clamav": False,
-            "file_size_limit": 500000000,
-            "filename_length_limit": 100,
-            "sanitization": True,
-            "sensitivity": 0.99,
-            "uploadlogs_mode": "blocked",
-            "whitelist": "RESTRICTIVE",
-            "whitelist_custom": [],
-        },
-    },
+    {},
 )
